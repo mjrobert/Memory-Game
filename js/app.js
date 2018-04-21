2aa57b883414bcfc30f3e0490c2c55ac2f948985
Matthew Robert <mjrobert@gmail.com>
2018-04-22 01:03:23 +0400

    feat: Cards flipping on click

--- js/app.js
+++ js/app.js
@@ -61,9 +61,6 @@ createDeck();
 
 
 
-
-
-
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -74,3 +71,9 @@ createDeck();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ deck.addEventListener('click', function (event) {
+    if (event.target.nodeName === 'LI') {  
+        event.target.classList.add("open", "show");
+    }
+});
